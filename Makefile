c36b5f45ff670921bd0c4c7ec5f6d24c19a44d9d
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2023-10-31 18:22:21 +0600

    layout main page

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 dev:
 	python3 manage.py runserver
+
+collectstatic:
+	python3 manage.py collectstatic --clear --ignore 'admin'
--- btr/settings.py
+++ btr/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'btr.auth',
-    "django_bootstrap5",
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -145,7 +145,11 @@ LANGUAGES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
