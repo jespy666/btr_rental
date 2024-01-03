2610f76743262b99c334dac6202aed36fdf3c42f
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-01-03 21:45:34 +0600

    add custom exceptions for tg bot

---
+++ btr/bookings/bot_exceptions.py
@@ -0,0 +1,26 @@
+class BusyDayException(Exception):
+    pass
+
+
+class TimeIsNotAvailable(Exception):
+    pass
+
+
+class InvalidTimeFormat(Exception):
+    pass
+
+
+class InvalidDateFormat(Exception):
+    pass
+
+
+class WrongBikeCount(Exception):
+    pass
+
+
+class InvalidEmailFormat(Exception):
+    pass
+
+
+class DateInPastException(Exception):
+    pass
