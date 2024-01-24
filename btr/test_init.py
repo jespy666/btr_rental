fbb2dcc00efaffa556ed15db8142fddd171e2689
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-01-24 18:50:55 +0600

    [test] ignore django signals

--- btr/test_init.py
+++ btr/test_init.py
@@ -1,17 +1,24 @@
+from django.db.models.signals import post_save
 from django.test import TestCase
 from django.urls import reverse_lazy
 from django.utils.translation import activate
 
-# from btr.bookings.models import Booking
+from btr.bookings.models import Booking
 from btr.users.models import SiteUser
+from btr.bookings.signals import booking_confirm_notify, create_booking_notify
 
 
 class BTRTestCase(TestCase):
 
+    post_save.disconnect(booking_confirm_notify, sender=Booking)
+    post_save.disconnect(create_booking_notify, sender=Booking)
+
     fixtures = [
         'btr/fixtures/users/users.json',
+        'btr/fixtures/bookings/bookings.json',
     ]
 
+    home_url = reverse_lazy('home')
     login_url = reverse_lazy('login')
     logout_url = reverse_lazy('logout')
 
@@ -20,7 +27,10 @@ class BTRTestCase(TestCase):
     count = 3
 
     def setUp(self):
-        self.existed_username = SiteUser.objects.get(pk=1)
-        self.existed_email = SiteUser.objects.get(pk=2)
-        self.existed_phone = SiteUser.objects.get(pk=3)
-        self.client.force_login(self.existed_username)
+        self.user = SiteUser.objects.get(pk=1)
+        self.user2 = SiteUser.objects.get(pk=2)
+        self.user3 = SiteUser.objects.get(pk=3)
+
+        self.booking = Booking.objects.get(pk=1)
+
+        self.client.force_login(self.user)
