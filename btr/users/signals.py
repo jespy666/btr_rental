5480fdfab78f17c32eff6076e688cda303a854eb
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-02-12 21:19:27 +0600

    add annotate types

--- btr/users/signals.py
+++ btr/users/signals.py
@@ -1,3 +1,4 @@
+from django.db.models import Model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext as _
@@ -6,7 +7,8 @@ from .models import SiteUser
 
 
 @receiver(post_save, sender=SiteUser)
-def set_status(sender, instance, created, **kwargs):
+def set_status(sender: Model, instance: SiteUser,
+               created: bool, **kwargs) -> None:
     """Set status to new users"""
     if created:
         instance.status = _('newbie')
