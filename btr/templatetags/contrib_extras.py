b0bc0d9bf5373dea039b766d0d34996ef49f79a2
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-02-27 17:30:19 +0600

    add common tags

---
+++ btr/templatetags/contrib_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_canonical_url(context):
+    """Get canonical url from request."""
+    request = context.get('request')
+    if request:
+        return request.build_absolute_uri(request.path)
+    return ''
