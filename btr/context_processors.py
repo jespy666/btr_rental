0284a0a5a671573f6e8627a7c89cc12932a2763b
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-03-01 19:33:01 +0600

    add common tags title and lang code

--- btr/context_processors.py
+++ btr/context_processors.py
@@ -1,8 +1,13 @@
 from django.conf import settings
+from django.utils.translation import gettext as _
 
 
 def common_context(request):
     """Set general context to templates"""
     return {
         'YANDEX_VERIFICATION_ID': settings.YANDEX_VERIFICATION_ID,
+        'title': _(
+            'BroTeamRacing - Enduro and Pit-bikes rental in Saint-Petersburg'
+        ),
+        'LANGUAGE_CODE': settings.LANGUAGE_CODE,
     }
