996f5bc764c2f608e3a6d688a7e45b451f56dd67
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-03-25 21:06:23 +0500

    do not change admin status

--- btr/bookings/signals.py
+++ btr/bookings/signals.py
@@ -10,14 +10,15 @@ from .models import Booking
 def update_user_status(sender: Model, instance: Booking, **kwargs) -> None:
     """Setting the user level based on the number of rides"""
     rider = instance.rider
-    book_count = rider.booking_set.filter(status='completed').count()
-    match book_count:
-        case count if count < 3:
-            rider.status = _('Newbie')
-        case count if 3 <= count < 5:
-            rider.status = _('Amateur')
-        case count if 5 <= count < 10:
-            rider.status = _('Professional')
-        case _:
-            rider.status = _('Master')
-    rider.save()
+    if not rider.is_superuser:
+        book_count = rider.booking_set.filter(status='completed').count()
+        match book_count:
+            case count if count < 3:
+                rider.status = _('Newbie')
+            case count if 3 <= count < 5:
+                rider.status = _('Amateur')
+            case count if 5 <= count < 10:
+                rider.status = _('Professional')
+            case _:
+                rider.status = _('Master')
+        rider.save()
