5d3a2317595d8d39537d5412568be1d5da7836a8
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-03-28 17:31:55 +0500

    add ru-genitive month name method

--- btr/templatetags/contrib_extras.py
+++ btr/templatetags/contrib_extras.py
@@ -1,4 +1,5 @@
 from django import template
+import datetime
 
 
 register = template.Library()
@@ -13,3 +14,22 @@ def get_canonical_url(context):
             request.path
         ).replace('http://', 'https://')
     return ''
+
+
+@register.filter
+def ru_month_genitive(date: datetime.date) -> str:
+    months_genitive = {
+        1: "Января",
+        2: "Февраля",
+        3: "Марта",
+        4: "Апреля",
+        5: "Мая",
+        6: "Июня",
+        7: "Июля",
+        8: "Августа",
+        9: "Сентября",
+        10: "Октября",
+        11: "Ноября",
+        12: "Декабря",
+    }
+    return months_genitive[date.month]
